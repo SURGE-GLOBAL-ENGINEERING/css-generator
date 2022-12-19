321578e1deae6c82e99fb0c4e3759a73c64e3276
Janith Gamage <gamage1995@gmail.com>
2022-12-19 14:33:16 +0530

    fix/Add epub previewer specific padding for ornamental breaks

--- cssGenerators/editorPlugins/ornamentalBreak.ts
+++ cssGenerators/editorPlugins/ornamentalBreak.ts
@@ -1,10 +1,15 @@
+import { EpubPreviewerPrefix } from "helpers"
+
+/**
+ * Can contain previewer only styles
+ */
 export const getOrnamentalBreakCss = (ornamentalBreakWidth: number, prefix: string) => {
   return `
     ${prefix}.ornamental-break{
       text-align: center;
       display: block;
       width: 100%;
-      padding: 0.8rem 0rem;
+      padding: ${prefix === EpubPreviewerPrefix ? '1.2rem' : '0.8rem'} 0rem;
     }
     
     ${prefix}.ornamental-break img{
