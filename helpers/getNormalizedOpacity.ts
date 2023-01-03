b6c1c57be466a6d5745e012b5963cb20bfe814e6
Janith Gamage <gamage1995@gmail.com>
2023-01-03 22:05:47 +0530

    bugfix/Update normalize image opacity funtion to handle 2 opacity scales

--- helpers/getNormalizedOpacity.ts
+++ helpers/getNormalizedOpacity.ts
@@ -1,4 +1,14 @@
+/**
+ * Normalizes and handles the possibility of having two opacity scales from 0-1 and 0-100
+ * @param opacity Image opacity in scale 0-1 or 0-100
+ * @returns Normalized opacity in scale 0-1
+ */
 export const getNormalizedOpacity = (opacity: number | undefined) => {
-  if (opacity !== undefined) return opacity;
-  return 100;
+  if (opacity !== undefined) {
+    if(opacity > 1){
+      return opacity/100;
+    }
+    return opacity;
+  }
+  return 1;
 };
