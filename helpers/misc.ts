3d3875b9da95847aa6e3c2080996748d1a919a1b
Janith Gamage <gamage1995@gmail.com>
2023-01-23 14:49:51 +0530

    bugfix/Set titleCard min-height to 80px for thumbnails

--- helpers/misc.ts
+++ helpers/misc.ts
@@ -20,6 +20,6 @@ export const thumbnailCssOverwrites = {
   title: () => `\npadding-top: 2px;\n padding-bottom: 2px;\n line-height: 1;\n width: 100%;\n font-size: 1em;\n ${slingleLineEllipsis()}`,
   subtitle: () => `\npadding-top: 2px;\n padding-bottom: 2px;\n line-height: 1;\n width: 100%;\n font-size: 0.8em;\n ${slingleLineEllipsis()}`,
   number: () => `\npadding-top: 2px;\n padding-bottom: 2px;\n line-height: 1;\n width: 100%;\n font-size: 0.8em;\n ${slingleLineEllipsis()}`,
-  titleCard: () => "\npadding-top: 2px;\n padding-bottom: 2px;\n min-height: unset;\n",
+  titleCard: () => "\npadding-top: 2px;\n padding-bottom: 2px;\n min-height: 80px;\n",
   imageContainer: () => "\npadding-top: 3px;\n padding-bottom: 2px;\n line-height: 0px;\n",
 }
