965e51d22c586ca75cfca0077b8c88c0d1fe5767
Ransara Wijayasundara <ransara@surge.global>
2023-03-07 15:20:36 +0530

    AT-1115 fix(epub-lists): updated the logic

--- cssGenerators/editorPlugins/list.ts
+++ cssGenerators/editorPlugins/list.ts
@@ -1,6 +1,6 @@
 /** li ol css in getDefaultCss.ts */
 export const getListPluginCss = (themeId: string, isPreviewer: boolean) => {
-  if (isPreviewer) {
+  if (!isPreviewer) {
     return `.${themeId} .list-parent{
       padding: 1rem;
     }`;
