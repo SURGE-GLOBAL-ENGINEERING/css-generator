6734f2b3f2d40fd62c94e070a42debc1e501731b
Mohamed Hassan Mohamed Althaf <hassan@surge.global>
2023-03-07 16:03:32 +0530

    AT-1150 - fix(dropcap-spacing): Fix dropcap spacing when succeeded by image on previewer

--- cssGenerators/getFirstParagraphCss.ts
+++ cssGenerators/getFirstParagraphCss.ts
@@ -1,4 +1,3 @@
-import { EpubPreviewerPrefix } from "../helpers";
 import { FirstParagraph, Paragraph } from "../types";
 
 /**
@@ -6,9 +5,7 @@ import { FirstParagraph, Paragraph } from "../types";
  */
 export const getFirstParagraphCss = (
   firstParagraph: FirstParagraph,
-  paragraph: Paragraph,
   themeId: string,
-  isPreviewer: boolean
 ) => {
   return `
     .${themeId} .withDropcap .dropcap, .${themeId} .ornamental-break + p{
@@ -49,15 +46,6 @@ export const getFirstParagraphCss = (
       .${themeId} .withDropcap .lead_word{
         text-transform: uppercase;
       }
-      `
-        : ""
-    }
-    ${
-      isPreviewer
-        ? `
-        .${themeId} .withDropcap .dropcap{
-          max-height: ${15 * 2}px;
-        }
       `
         : ""
     }
