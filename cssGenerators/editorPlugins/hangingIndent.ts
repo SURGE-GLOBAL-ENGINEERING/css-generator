70a17a65fca849b7c3579f3326c5f5c9a646e4d5
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-06-22 22:32:24 +0530

    style/AT-1535 change rem to inches (to achieve 0.5 inch indent)

--- cssGenerators/editorPlugins/hangingIndent.ts
+++ cssGenerators/editorPlugins/hangingIndent.ts
@@ -4,8 +4,8 @@ export const getHangingIndentCss = (themeId: string) => {
         line-height: 1.6em;
         padding-top: 0.75rem;
         padding-bottom: 0.75rem;
-        text-indent: -3rem;
-        padding-left: 3rem;
+        text-indent: -0.5in;
+        padding-left: 0.5in;
       }
     `
   };
