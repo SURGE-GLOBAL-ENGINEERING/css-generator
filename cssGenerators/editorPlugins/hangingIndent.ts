00fc4caac5f9a1b0bf528a69c824877b5157500e
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-07-26 16:18:49 +0530

    bugfix/AT-1535 remove top and bottom paddings

--- cssGenerators/editorPlugins/hangingIndent.ts
+++ cssGenerators/editorPlugins/hangingIndent.ts
@@ -2,8 +2,6 @@ export const getHangingIndentCss = (themeId: string) => {
     return `
       .${themeId} .hanging{
         line-height: 1.6em;
-        padding-top: 0.75rem;
-        padding-bottom: 0.75rem;
         text-indent: -0.25in;
         padding-left: 0.25in;
         orphans: 3; 
