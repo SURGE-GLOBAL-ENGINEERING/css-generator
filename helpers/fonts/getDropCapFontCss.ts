5af6d3e5e12e65d4ac4965bbaf12a04f57226ede
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-09-06 21:14:44 +0530

    feat/AT-1662 "FrederickatheGreat": update font sizes of epub previewer and export

--- helpers/fonts/getDropCapFontCss.ts
+++ helpers/fonts/getDropCapFontCss.ts
@@ -3,7 +3,7 @@ export const getDropCapFontCss = (fontFamily: string, isPreviewer: boolean) => {
   switch(fontFamily) {
     case ("FrederickatheGreat"): 
       drop = `margin-right: 1rem;\n margin-left: 0.7rem;\n padding: 0.25rem 0.25rem 0rem;\n `;
-      isPreviewer ? drop += `font-size: 4rem;\n ` : drop+=`font-size: 3.4rem;\n `;
+      isPreviewer ? drop += `font-size: 4.1rem;\n ` : drop+=`font-size: 3.6rem;\n `;
       return drop;
     default: return `margin-right: 0.125rem;\n padding: 0 0.25rem;\n `;
   }
