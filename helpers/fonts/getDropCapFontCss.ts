f522dbd4e1454ff44d0cb7548d767bfd93b9fea9
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-09-11 18:39:46 +0530

    refactor/AT-1662 "FrederickatheGreat": update margins of epub previewer and export

--- helpers/fonts/getDropCapFontCss.ts
+++ helpers/fonts/getDropCapFontCss.ts
@@ -2,7 +2,7 @@ export const getDropCapFontCss = (fontFamily: string, isPreviewer: boolean) => {
   let drop = ``;
   switch(fontFamily) {
     case ("FrederickatheGreat"): 
-      drop = `margin-right: 1rem;\n margin-left: 0.7rem;\n padding: 0.25rem 0.25rem 0rem;\n `;
+      drop = `margin-right: 0.8rem;\n margin-left: 0.46rem;\n padding: 0.25rem 0.25rem 0rem;\n `;
       isPreviewer ? drop += `font-size: 4.1rem;\n ` : drop+=`font-size: 3.6rem;\n `;
       return drop;
     default: return `margin-right: 0.125rem;\n padding: 0 0.25rem;\n `;
