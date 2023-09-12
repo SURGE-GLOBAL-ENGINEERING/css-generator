faa75326fd48387041d5a600082d6bed1c05b5c8
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-09-13 00:59:00 +0530

    feat/AT-1662 "ArchitectsDaughter": epub previewer and export tuning configs

--- helpers/fonts/getDropCapFontCss.ts
+++ helpers/fonts/getDropCapFontCss.ts
@@ -5,6 +5,10 @@ export const getDropCapFontCss = (fontFamily: string, isPreviewer: boolean) => {
       drop = `margin-right: 0.8rem;\n margin-left: 0.46rem;\n padding: 0.25rem 0.25rem 0rem;\n `;
       isPreviewer ? drop += `font-size: 4.1rem;\n ` : drop+=`font-size: 3.6rem;\n `;
       return drop;
+      case ("ArchitectsDaughter"):
+        drop = `margin-right: 0.35rem;\n padding: 0 0.25rem;\n `;
+        isPreviewer ? drop += `font-size: 3rem;\n ` : drop+=`font-size: 2.9rem;\n `;
+        return drop;
     default: return `margin-right: 0.125rem;\n padding: 0 0.25rem;\n `;
   }
 };
