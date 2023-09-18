0901dc9cd5e03d08ceb4712418c2ee2149133916
Janith Gamage <gamage1995@gmail.com>
2023-09-18 14:25:22 +0530

    chore/release-it Extend release-it with a .sh script

--- release-package.sh
+++ release-package.sh
@@ -1,67 +1,30 @@
 #!/usr/bin/env bash
 
-type="default"
-stage="default"
+# Prompt the user for the 'type' variable
+read -p "Enter the release type (patch, minor, major): " type
 
-# Function to validate the type flag
-validate_type() {
-     if [ -z "$1" ]; then
-        echo "Type can not be empty"
+# Validate the 'type' input
+case "$type" in
+    patch|minor|major)
+        ;;
+    *)
+        echo "Invalid release type: $type"
         exit 1
-    fi
+        ;;
+esac
 
-    case "$1" in
-        patch|minor|major)
-            ;;
-        *)
-            echo "Invalid type: $1; Expected type to be patch, minor or major"
-            exit 1
-            ;;
-    esac
-}
+# Prompt the user for the 'stage' variable
+read -p "Enter the release stage (dev, qa, main): " stage
 
-# Function to validate the stage flag
-validate_stage() {
-    if [ -z "$1" ]; then
-        echo "Stage can not be empty"
+# Validate the 'stage' input
+case "$stage" in
+    dev|qa|main)
+        ;;
+    *)
+        echo "Invalid release stage: $stage"
         exit 1
-    fi
-    case "$1" in
-        dev|qa|main)
-            ;;
-        *)
-            echo "Invalid stage: $1; Expected stage to be dev, qa or main"
-            exit 1
-            ;;
-    esac
-}
-
-# Parse command-line arguments
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        --type)
-            shift
-            validate_type "$1"
-            type="$1"
-            ;;
-        --stage)
-            shift
-            validate_stage "$1"
-            stage="$1"
-            ;;
-        *)
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-    esac
-    shift
-done
-
-# Check if both type and stage flags have been provided
-if [[ "$type" == "default" || "$stage" == "default" ]]; then
-    echo "Both type and stage arguments are mandatory."
-    exit 1
-fi
+        ;;
+esac
 
 releaseType=""
 preRelease=""
@@ -85,12 +48,6 @@ case "$stage" in
         releaseType=$type
         ;;
 esac
-
-echo "Type flag is set to: $type"
-echo "Stage flag is set to: $stage"
-
-echo "release type: $releaseType"
-echo "prerelease: $preRelease"
 
 release-it --dry-run $releaseType --git.requireBranch=release-it-extended $preRelease
 
