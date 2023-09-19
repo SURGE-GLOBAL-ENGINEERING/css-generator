171c99139d80ae76d21c08f19fd80edf46244dde
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2023-09-19 12:52:54 +0530

    feat/AT-1662 "BerkshireSwash": epub previewer and export tuning configs

--- helpers/fonts/getDropCapCssClasses.ts
+++ helpers/fonts/getDropCapCssClasses.ts
@@ -51,6 +51,74 @@ const getDropCapFontCss = (fontFamily: string, letter: string, isPreviewer: bool
                 padding: "0.25rem 0.45rem 0rem",
             }
         },
+        BerkshireSwash: {
+            C: {
+                "font-size": "2.2rem",
+                "margin-right": "0.425rem",
+                padding: "0.15rem 0.25rem 0rem",
+            },
+            G: {
+                "font-size": "2rem",
+                "margin-left": "-0.18rem",
+                "margin-right": "0.325rem",
+                padding: "0.18rem 0.25rem 0rem"
+            },
+            H: {
+                "font-size": "1.8rem",
+                padding: "0.4rem 0.25rem 0rem",
+                "margin-right": "0.45rem",
+            },
+            J: {
+                "font-size": "2.1rem",
+                "margin-right": "0.2rem",
+                padding: "0.18rem 0.25rem 0rem"
+            },
+            K: {
+                "font-size": "2.1rem",
+                "margin-left": "-0.1rem",
+                "margin-right": "0.55rem",
+                padding: "0.2rem 0.25rem 0rem"
+            },
+            M: {
+                "font-size": "2.1rem",
+                padding: "0.5rem 0.25rem 0rem",
+                "margin-right": "0.4rem",
+            },
+            N: {
+                "font-size": "2.1rem",
+                padding: "0.2rem 0.25rem 0rem",
+                "margin-right": "0.525rem",
+            },
+            Q: {
+                "font-size": "2.1rem",
+                "margin-left": "-0.2rem",
+                "margin-right": "0.4rem",
+                padding: "0.1rem 0.25rem 0rem",
+            },
+            R: {
+                "font-size": "2.15rem",
+                "margin-left": "-0.1rem",
+                "margin-right": "0.55rem",
+                padding: "0.15rem 0.25rem 0rem",
+            },
+            S: {
+                "font-size": "2.2rem",
+                "margin-left": "-0.15rem",
+                "margin-right": "0.225rem",
+                padding: "0.2rem 0.25rem 0rem",
+            },
+            Y: {
+                "font-size": "1.9rem",
+                "margin-left": "0.09rem",
+                "margin-right": "0.5rem",
+                padding: "0.2rem 0.25rem 0rem",
+            },
+            default: {
+                "font-size": "2.4rem",
+                "margin-right": "0.35rem",
+                padding: "0.18rem 0.25rem 0rem",
+            }
+        },
     };
 
     const fontFamilyStyles = fontStyles[fontFamily] || {};
