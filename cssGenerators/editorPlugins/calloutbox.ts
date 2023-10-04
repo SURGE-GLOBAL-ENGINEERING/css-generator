1ae503c064a4fca56d7e02d53f9fd62350227e09
manula uluwatta <manulau97@gmail.com>
2023-10-04 11:51:55 +0530

    refactor/AT-1819 removed unnecessary styles

--- cssGenerators/editorPlugins/calloutbox.ts
+++ cssGenerators/editorPlugins/calloutbox.ts
@@ -2,17 +2,7 @@ export const getCalloutBoxCss = (themeId: string) => {
 	return `
       .${themeId} .calloutbox {
         margin-top: 5px;
-        border: 2px solid rgb(52, 51, 51);
-        background-color: #faffef;
         padding: 1em 1em 1em 1em;
-      }
-
-      .${themeId} .calloutbox-callout-unfilled{
-        background-color: transparent !important;
-      }
-
-      .${themeId} .calloutbox-callout-no-border{
-        border: none !important;
       }
     `;
 };
