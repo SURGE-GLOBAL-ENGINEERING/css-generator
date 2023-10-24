907e26350bf02fd7fdcf812f77f99bd609a4d8ba
Maryam-Yumna <maryamyumna@gmail.com>
2023-10-24 19:23:36 +0530

    AT-2171 3Dumb dropcap font calculations

--- helpers/fonts/getDropCapCssClasses.ts
+++ helpers/fonts/getDropCapCssClasses.ts
@@ -179,6 +179,37 @@ const getDropCapFontCss = (fontFamily: string, letter: string, isPreviewer: bool
                 padding: "0.18rem 0.25rem 0rem",
             }
         },
+        "3Dumb": {
+            R: {
+              "font-size": isPreviewer ? "2.45rem" : "2.4rem",
+            },
+            S: {
+              "font-size": isPreviewer ? "2.3rem" : "2.3rem",
+              padding: "0.15rem 0.25rem 0rem",
+            },
+            T: {
+              "font-size": isPreviewer ? "2.45rem" : "2.4rem",
+            },
+            U: {
+              "font-size": isPreviewer ? "2.5rem" : "2.4rem",
+            },
+            W: {
+              "font-size": isPreviewer ? "2.4rem" : "2.2rem",
+            },
+            X: {
+              "font-size": isPreviewer ? "2.5rem" : "2.45rem",
+            },
+            Y: {
+              "font-size": isPreviewer ? "2.4rem" : "2.35rem",
+            },
+            Z: {
+              "font-size": isPreviewer ? "2.4rem" : "2.35rem",
+            },
+            default: {
+              "font-size": isPreviewer ? "2.4rem" : "2.3rem",
+              padding: "0.1rem 0.25rem 0rem",
+            },
+        },
     };
 
     const fontFamilyStyles = fontStyles[fontFamily] || {};
@@ -197,8 +228,10 @@ export const getDropCapCssClasses = (fontFamily: string, prefixRule: string, isP
 
     for (let letter of alphabet) {
         const fontRule = getDropCapFontCss(fontFamily, letter, isPreviewer);
+        console.log("fontRulle", fontRule);
         if (fontRule) {
             cssString += `.${prefixRule} .withDropcap .dropcap.letter_${letter} {\n  ${fontRule}\n}\n`;
+            console.log("string", cssString);
         }
     }
 
