653cc421a87c070f3173346ef1056271bcd957fe
manula uluwatta <manulau97@gmail.com>
2023-11-07 09:54:07 +0530

    fix/AT-2120 added box-sizing

--- cssGenerators/editorPlugins/calloutbox.ts
+++ cssGenerators/editorPlugins/calloutbox.ts
@@ -1,6 +1,7 @@
 export const getCalloutBoxCss = (themeId: string) => {
 	return `
       .${themeId} .calloutbox {
+        box-sizing: border-box;
         margin-top: 5px;
         padding: 1em 1em 1em 1em;
         display: inline-block;
