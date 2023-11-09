30bc2ffc0175c8a0e7fa200853e7b5a4269932c5
manula uluwatta <manulau97@gmail.com>
2023-11-09 19:42:14 +0530

    fix/AT-2295 set margin-bottom calloutbox

--- cssGenerators/editorPlugins/calloutbox.ts
+++ cssGenerators/editorPlugins/calloutbox.ts
@@ -3,6 +3,7 @@ export const getCalloutBoxCss = (themeId: string) => {
       .${themeId} .calloutbox {
         box-sizing: border-box;
         margin-top: 5px;
+        margin-bottom: 5px;
         padding: 1em 1em 1em 1em;
         display: inline-block;
         width: 100%;
